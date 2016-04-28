0129f0f72d72227e6000e90c5f16f5ddf6bdda96
Matt Coleman <hello@matt-coleman.co.uk>
2016-04-28 14:34:04 +0100

    Removed redundant oTransitionEnd

--- modal.js
+++ modal.js
@@ -141,7 +141,6 @@
   function transitionSelect() {
     var el = document.createElement("div");
     if (el.style.WebkitTransition) return "webkitTransitionEnd";
-    if (el.style.OTransition) return "oTransitionEnd";
     return 'transitionend';
   }
 
