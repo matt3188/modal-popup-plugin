83208814175dd0047cf0fe1d3e1570fbdb8643f8
Matt Coleman <hello@matt-coleman.co.uk>
2016-04-28 14:18:46 +0100

    Created the private buildOut function to actual build the modal

--- modal.js
+++ modal.js
@@ -41,4 +41,55 @@
     }
     return source;
   }
+
+  function buildOut() {
+
+    var content, contentHolder, docFrag;
+
+    // If content is an HTML string, append to the HTML string
+    // If content is a domNode. append its content
+    
+    if ( typeof this.options.content === "string" ) {
+      content = this.options.content;
+    } else {
+      content = this.options.content.innerHTML;
+    }
+
+    // Create a DocumentFragment to build with
+    docFrag = document.createDocumentFragment();
+
+    // Create modal element
+    this.modal = document.createElement( 'div' );
+    this.modal.className = 'custom-modal ' + this.options.className;
+    this.modal.style.minWidth = this.options.minWidth + 'px';
+    this.modal.style.maxWidth = this.options.maxWidth + 'px';
+
+    // If closeButton option is true, add a close button
+    if ( this.options.closeButton === true ) {
+      this.closeButton = document.createElement( 'button' );
+      this.closeButton.className = 'btn close-btn';
+      this.closeButton.innerHTML = 'x';
+      this.modal.appendChild( this.closeButton );
+    }
+
+    // If overlay is true, add one
+    if( this.options.overlay === true ) {
+      this.overlay = document.createElement( 'div' );
+      this.overlay.className = 'modal-overlay ' + this.options.className;
+      docFrag.appendChild( this.overlay );
+    }
+
+    // Create content area and append to modal
+    contentHolder = document.createElement( 'div' );
+    contentHolder.className = 'modal-content';
+    contentHolder.innerHTML = content;
+    this.modal.appendChild( contentHolder );
+
+    // Append modal to DocumentFrag
+    docFrag.appendChild( this.modal );
+
+    // Append DocumentFrag to body
+    document.body.appendChild( docFrag );
+  }
+
 })();
